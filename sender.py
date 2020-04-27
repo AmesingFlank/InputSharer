79ef5d82f2f8d468ee7d0bbb3def4f6541ce1049
AmesingFlank <ldfrank533@outlook.com>
2020-04-27 23:43:35 +0100

    add sender file

---
+++ sender.py
@@ -0,0 +1,38 @@
+from pynput import keyboard
+
+
+import signal
+import sys
+
+def signal_handler(sig, frame):
+    pass
+signal.signal(signal.SIGINT, signal_handler)
+
+
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
